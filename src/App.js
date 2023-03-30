4af800a6566501601d5a779d0fe27900378d431c
personal <devesh.lakwal911@gmail.com>
2023-03-30 22:00:37 +0530

    final changes4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and firebase
+          Learn React and firebase and rentify
         </a>
       </header>
     </div>
